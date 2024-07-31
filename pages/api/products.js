c1302ae8e1d7a6a324cbe33d61bdd95ec6e01fa8
Yahor Ramanenka <yahor.ramanenka@gmail.com>
2024-07-31 22:29:55 +0200

    remove obsolete wrong api

--- pages/api/products.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const products = fs.readFileSync("products.json").toString("utf-8");
-
-module.exports = function (req, res) {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(products);
-};
