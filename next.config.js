f4d3b598a9b9c08e65628cdbd804ec3ba1580838
Yahor Ramanenka <yahor.ramanenka@gmail.com>
2024-08-01 13:55:45 +0200

    add redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
